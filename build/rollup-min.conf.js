2b851ec4c014e377d1de0d748ca84cb36ece3433
shrpne <shrpne@gmail.com>
2019-03-02 01:03:18 +0300

    update deps

--- build/rollup-min.conf.js
+++ build/rollup-min.conf.js
@@ -1,13 +1,13 @@
 
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import {terser} from 'rollup-plugin-terser';
 
 
 export default {
     input: 'src/index.js',
     plugins: [
         babel(),
-        uglify(),
+        terser(),
     ],
     output: [
         {
